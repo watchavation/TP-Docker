373df082bb7698094facc81d2d970da9480f699f
watchavation <dumont.paul@cfa-afti.fr>
2019-05-04 21:05:34 +0200

    feat : ajout des Dockerfile et modificaitond des scripts

--- api-backend/api.py
+++ api-backend/api.py
@@ -31,4 +31,4 @@ def traceback():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8001)
+    app.run(debug=False, host='0.0.0.0', port=8001)
--- api-frontend/api.py
+++ api-frontend/api.py
@@ -52,4 +52,4 @@ def traceback():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=False, host='0.0.0.0', port=8000)
